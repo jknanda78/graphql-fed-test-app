1775fbfb131a3441828ffacf5410eb91428012bf
j0n016w <jyoti.nanda@walmart.com>
2025-10-25 17:10:08 -0700

    chore: added voyager for schema

--- user-subgraph/src/server.ts
+++ user-subgraph/src/server.ts
@@ -1,32 +1,72 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { buildSubgraphSchema } from "@apollo/subgraph";
-import resolvers from "@user-subgraph/resolver";
+import { ApolloServer } from '@apollo/server';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+// import { startStandaloneServer } from '@apollo/server/standalone';
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import resolvers from '@user-subgraph/resolver';
+import http from 'http';
+import express from 'express';
+import { expressMiddleware } from '@as-integrations/express5';
 
-import { readFileSync } from "fs";
-import gql from "graphql-tag";
+import { readFileSync } from 'fs';
+import gql from 'graphql-tag';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
 
 const typeDefs = gql(readFileSync('./user-subgraph/src/schema.graphql', { encoding: 'utf-8' }));
 
 async function startApolloServer() {
-  const server = new ApolloServer({
-    schema: buildSubgraphSchema({ typeDefs, resolvers }),
-  });
+  // const server = new ApolloServer({
+  //   schema: buildSubgraphSchema({ typeDefs, resolvers }),
+  // });
 
   const port = 4001;
   const subgraphName = 'user';
 
-  try {
-    const { url } = await startStandaloneServer(server, {
-      context: async () => {
-        return {
-          dataSources: {},
-        };
-      },
-      listen: { port },
-    });
+  // Optional: Set up Voyager middleware for schema visualization
+  // Required logic for integrating with Express
+  const app = express();
+  // Our httpServer handles incoming requests to our Express app.
+  // Below, we tell Apollo Server to "drain" this httpServer,
+  // enabling our servers to shut down gracefully.
+  const httpServer = http.createServer(app);
+  // Same ApolloServer initialization as before, plus the drain plugin
+  // for our httpServer.
+  const server = new ApolloServer({
+    schema: buildSubgraphSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
 
-    console.log(`🚀 Subgraph ${subgraphName} running at ${url}`);
+  app.use('/voyager', voyagerMiddleware({ endpointUrl: '/graphql' }));
+
+  try {
+    // const { url } = await startStandaloneServer(server, {
+    //   context: async () => {
+    //     return {
+    //       dataSources: {},
+    //     };
+    //   },
+    //   listen: { port },
+    // });
+
+    // Ensure we wait for our server to start
+    await server.start();
+
+    // Set up our Express middleware to handle CORS, body parsing,
+    // and our expressMiddleware function.
+    app.use(
+      '/graphql',
+      // cors(),
+      // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+      express.json({ limit: '50mb' }),
+      // expressMiddleware accepts the same arguments:
+      // an Apollo Server instance and optional configuration options
+      expressMiddleware(server, {
+        context: async ({ req }) => ({ token: req.headers.token, dataSources: {} }),
+      })
+    );
+
+    // Modified server startup
+    await new Promise<void>(resolve => httpServer.listen({ port }, resolve));
+    console.log(`🚀 Subgraph ${subgraphName} running at ${port}`);
   } catch (err) {
     console.error(err);
   }
